9bc2d0d239e7e261b983dc80ad86ecf07ed56605
Anthony Medina <anthonymedina958@gmail.com>
2023-04-30 05:26:23 -0700

    update

--- helpers/mapbox.js
+++ helpers/mapbox.js
@@ -3,6 +3,7 @@ const getMapboxAccessToken = process.env.MAPBOX_ACCESS_TOKEN
 
 exports.geocoding = async (value='', types=['place'], limit=1) => {
     let _value = value.replace(/_/g, '#')
+    
     return fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${_value.replace(/#/g,"")}.json?country=US&types=${encodeURIComponent(types)}&limit=${limit}&access_token=${getMapboxAccessToken}`).then(res => res.json())
     .then(json => json);;
 }
